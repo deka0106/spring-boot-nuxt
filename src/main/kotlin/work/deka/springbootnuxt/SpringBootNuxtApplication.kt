8dd1d6cb263c21daee97bf2d21285a78c82d0dcc
Deka <dekachan16@gmail.com>
2019-07-06 13:23:47 +0900

    Initialize Spring Boot Project

---
+++ src/main/kotlin/work/deka/springbootnuxt/SpringBootNuxtApplication.kt
@@ -0,0 +1,11 @@
+package work.deka.springbootnuxt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootNuxtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootNuxtApplication>(*args)
+}
